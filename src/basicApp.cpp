6db7fb7b9855e0421a5b515c107d2ee244d3b807
Bartek Banachewicz <bananu7@o2.pl>
2014-03-06 23:06:10 +0100

    Fixed those silly parens because they are hellishly annoying

--- src/basicApp.cpp
+++ src/basicApp.cpp
@@ -8,36 +8,36 @@ using namespace std;
 // We'll create a new Cinder Application by deriving from the AppBasic class
 class BasicApp : public AppBasic {
   public:
-	void mouseDrag( MouseEvent event );
-	void keyDown( KeyEvent event );
-	void draw();
+    void mouseDrag(MouseEvent event);
+    void keyDown(KeyEvent event);
+    void draw();
 
-	// This will maintain a list of points which we will draw line segments between
-	list<Vec2f>		mPoints;
+    // This will maintain a list of points which we will draw line segments between
+    list<Vec2f> points;
 };
 
-void BasicApp::mouseDrag( MouseEvent event )
+void BasicApp::mouseDrag(MouseEvent event)
 {
-	mPoints.push_back( event.getPos() );
+    points.push_back(event.getPos());
 }
 
-void BasicApp::keyDown( KeyEvent event )
+void BasicApp::keyDown(KeyEvent event)
 {
-	if( event.getChar() == 'f' )
-		setFullScreen( ! isFullScreen() );
+    if(event.getChar() == 'f')
+        setFullScreen( ! isFullScreen());
 }
 
 void BasicApp::draw()
 {
-	gl::clear( Color( 0.1f, 0.1f, 0.15f ) );
+    gl::clear(Color(0.1f, 0.1f, 0.15f));
 
-	gl::color( 1.0f, 0.5f, 0.25f );	
-	gl::begin( GL_LINE_STRIP );
-	for( auto pointIter = mPoints.begin(); pointIter != mPoints.end(); ++pointIter ) {
-		gl::vertex( *pointIter );
-	}
-	gl::end();
+    gl::color( 1.0f, 0.5f, 0.25f);
+    gl::begin(GL_LINE_STRIP);
+    for (auto point : points) {
+        gl::vertex(point);
+    }
+    gl::end();
 }
 
 // This line tells Cinder to actually create the application
-CINDER_APP_BASIC( BasicApp, RendererGl )
+CINDER_APP_BASIC(BasicApp, RendererGl)
